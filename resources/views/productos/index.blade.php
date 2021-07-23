25fce275d51ddc8f55e5a1909447b94c311fecd2
dertavo <gderiangarcia@gmail.com>
2021-07-22 21:47:50 -0500

    Modal para los detalles del producto

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -37,14 +37,26 @@
 
     @foreach($productos as $producto)
 
-    <tr>
+    @php 
+    $activo="";
+
+    if($producto->estado=="Inactivo"){
+        $activo="alert alert-danger";
+    }
+
+
+    @endphp
+
+    <tr class="<?php echo $activo ?>"> 
       <th>{{$producto->sku}}</th>
       <td>{{$producto->nombre_producto}}</td>
       <td>{{$producto->cantidad}}</td>
       <td>{{$producto->precio}}</td>
       <td>{{$producto->estado}}</td>
       <td>
-      <a class="btn btn-success" href="#">Ver detalles</a>
+      <a id="detalles" class="btn btn-success" href="#" 
+      data-sku="{{$producto->sku}}"
+      data-toggle="modal" data-target="#myModal">Ver detalles</a>
           <a class="btn btn-secondary" href="{{route('productos.edit',$producto->id)}}">Editar</a>
           <form action="{{route('productos.destroy',['producto'=>$producto->id])}}" method="post">
          @csrf
@@ -60,9 +72,46 @@
   </tbody>
 </table>
 
+<div class="modal" tabindex="-1" id="myModal"> 
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Detalles del producto</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <label for="">SKU</label>
+        <input type="text" id="SKU" class="form-control"> 
 
+      </div>
+      <div class="modal-footer">
+      
+        <button type="button" class="btn btn-primary">Entendido</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 </div>
 
+
+@section('scripts')
+
+
+<script>
+
+$('#myModal').on('shown.bs.modal', function (event) {
+ 
+    var elem = document.getElementById('detalles');
+    var sku = elem.getAttribute('data-sku');
+    
+    $('#SKU').val(sku);
+})
+
+</script>
+
+@endsection
 
 @endsection
